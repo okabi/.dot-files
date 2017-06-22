adffcccd43d75bb0e6e6dd8fbc988e46ec286baa
okabi <okabi13@yahoo.co.jp>
2017-06-22 23:00:24 +0900

    HTMLメジャーモード web-mode の追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -265,3 +265,27 @@
 ;; インデント方式の変更
 (custom-set-variables
  '(haskell-mode-hook '(turn-on-haskell-indentation)))
+
+
+;;; HTML
+;; HTML 用メジャーモード
+(when (require 'web-mode nil t)
+  ;; emacs 23以下の互換
+  (when (< emacs-major-version 24)
+    (defalias 'prog-mode 'fundamental-mode))
+
+  ;; 適用する拡張子
+  (add-to-list 'auto-mode-alist '("\\.phtml$"     . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsp$"       . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
+
+  ;; インデント数
+  (defun web-mode-hook ()
+    "Hooks for Web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-offset 2))
+  (add-hook 'web-mode-hook 'web-mode-hook))
